abbadc217b46b5a27d035f0b4f94e667e106a63c
Thunderinho <k.stach1998@gmail.com>
2021-09-29 17:09:12 +0200

    Add project files.

---
+++ AdvancedProgramming_Lesson3/wwwroot/site.js
@@ -0,0 +1,110 @@
+﻿const uri = "https://localhost:44354/api/TodoItems";
+let todos = null;
+function getCount(data) {
+    const el = $("#counter");
+    let name = "to-do";
+    
+}
+$(document).ready(function () {
+    getData();
+});
+function getData() {
+    $.ajax({
+        type: "GET",
+        url: uri,
+        cache: false,
+        success: function (data) {
+            const tBody = $("#todos");
+            $(tBody).empty();
+            getCount(data.length);
+            $.each(data, function (key, item) {
+                const tr = $("<tr></tr>")
+                    .append($("<td></td>").text("Pozycja numer:",item.id))
+                    .append($("<td></td>").text(item.name))
+                    .append(
+                        $("<td></td>").append(
+                            $("<button>Edytuj</button>").on("click", function () {
+                                editItem(item.id);
+                            })
+                        )
+                    )
+                    .append(
+                        $("<td></td>").append(
+                            $("<button>Usuń</button>").on("click", function () {
+                                deleteItem(item.id);
+                            })
+                        )
+                    );
+                tr.appendTo(tBody);
+            });
+            todos = data;
+        }
+    });
+}
+function addItem() {
+    const item = {
+        name: $("#add-name").val(),
+    };
+    $.ajax({
+        type: "POST",
+        accepts: "application/json",
+        url: uri + '/CreateTodoItem',
+        contentType: "application/json",
+        data: JSON.stringify(item),
+        error: function (jqXHR, textStatus, errorThrown) {
+            alert("Something went wrong!");
+        },
+        success: function (result) {
+            getData();
+            $("#add-name").val("");
+        }
+    });
+}
+
+function deleteItem(id) {
+    $.ajax({
+        url: uri + "/" + id,
+        type: "DELETE",
+        success: function (result) {
+            getData();
+        }
+    });
+}
+
+function editItem(id) {
+    $.each(todos, function (key, item) {
+        if (item.id === id) {
+            $("#edit-name").val(item.name);
+      
+
+        }
+    });
+    $("#spoiler").css({ display: "block" });
+}
+
+function updateItem() {
+    var id = parseInt($("#edit-id").val(), 10);
+    const item = {
+        id: id,
+        name: $("#edit-name").val(),
+      s
+    };
+    $.ajax({
+        type: "POST",
+        accepts: "application/json",
+        url: uri + '/UpdateTodoItem',
+        contentType: "application/json",
+        data: JSON.stringify(item),
+        error: function (jqXHR, textStatus, errorThrown) {
+            alert("Something went wrong!");
+        },
+        success: function (result) {
+            getData();
+            closeInput();
+        }
+    });
+}
+
+function closeInput() {
+    $("#spoiler").css({ display: "none" });
+}
